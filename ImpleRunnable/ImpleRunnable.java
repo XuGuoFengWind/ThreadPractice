bba732b524ecdc473d0fe74fa0fb67230e07e72d
Like Wind <34294454+XuGuoFengWind@users.noreply.github.com>
2017-12-14 20:28:21 +0800

    Update ImpleRunnable.java

--- ImpleRunnable/ImpleRunnable.java
+++ ImpleRunnable/ImpleRunnable.java
@@ -4,7 +4,7 @@ class MusicImple implements Runnable{
 	public void run() {
 		for(int i=0;i<50;i++)
 		{
-			System.out.println("ÌýÒôÀÖ"+i);
+			System.out.println("å¬éŸ³ä¹"+i);
 		}	
 	}
 	
@@ -12,7 +12,7 @@ class MusicImple implements Runnable{
 public class ImpleRunnable {
 	public static void main(String[] args) {
 		for(int i=0;i<50;i++) {
-			System.out.println("´òÓÎÏ·"+i);
+			System.out.println("æ‰“æ¸¸æˆ"+i);
 			if(i==10)
 			{
 				Runnable mus=new MusicImple();
